c0012346450345a21cd616010bdbe0e7ff3bf8a2
Kamil Badyla <kamilbadyla@gmail.com>
2018-05-24 17:49:10 +0200

    export type fix

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 import { NativeModules } from 'react-native';
 
-export interface RemuxerInterface {
+interface RemuxerInterface {
     convert(inputFilePaths: string, outputFilePath: string): Promise<string>;
 }
 
 const RNReactNativeTsToMp4: RemuxerInterface = NativeModules.RNReactNativeTsToMp4;
+
+export type { RemuxerInterface };
 export default RNReactNativeTsToMp4;
