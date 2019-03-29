611cc1a38a69118b244484e73cd38a6bca5f3bda
Rigellute <rigellute@gmail.com>
2019-03-29 15:40:49 +0000

    Fix syntax error when exporting interface

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 
 const rn = require('react-native');
 
-interface RemuxerInterface {
+export interface RemuxerInterface {
     convert(inputFilePaths: string, outputFilePath: string, options: { oauthToken: string, ffmpegBinaryURL: string }): Promise<string>;
 }
 
 const RNReactNativeTsToMp4: RemuxerInterface = rn.NativeModules.RNReactNativeTsToMp4;
 
-export type { RemuxerInterface };
 module.exports = RNReactNativeTsToMp4;
